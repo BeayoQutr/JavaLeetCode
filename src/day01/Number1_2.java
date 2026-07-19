1cbe5b9111e6e236fcc04607b451e4691db962cd
beayoqutr <beayoqutr@gmail.com>
2026-07-19 22:35:35 +0800

    哈希表解法

--- src/day01/Number1_2.java
+++ src/day01/Number1_2.java
@@ -5,15 +5,13 @@ import java.util.Map;
 
 public class Number1_2 {
     public int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> indexByValue = new HashMap<>();
+        Map<Integer,Integer> map = new HashMap<>();
         for (int i = 0; i < nums.length; i++) {
-            int complement = target - nums[i];
-            Integer previousIndex = indexByValue.get(complement);
-            if (previousIndex != null) {
-                return new int[] {previousIndex, i};
+            if(map.containsKey(target - nums[i])){
+                return new int[]{map.get(target - nums[i]), i};
             }
-            indexByValue.put(nums[i], i);
+            map.put(nums[i], i);
         }
-        throw new IllegalArgumentException("No solution");
+        return new int[]{};
     }
 }
