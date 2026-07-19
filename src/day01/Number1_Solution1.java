d21ac7fe71a593a71a229f3e51e93aed1f9646cc
beayoqutr <beayoqutr@gmail.com>
2026-07-19 22:58:00 +0800

    暴力解法

--- src/day01/Number1_Solution1.java
+++ src/day01/Number1_Solution1.java
@@ -1,6 +1,6 @@
 package day01;
 
-public class Number1_1 {
+public class Number1_Solution1 {
     public int[] twoSum(int[] nums, int target) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
