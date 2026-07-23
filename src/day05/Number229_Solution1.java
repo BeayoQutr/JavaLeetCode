62a2a20ad35dd6c6d33adb3433d0fe04fcad7163
岁年 <beayoqutr@gmail.com>
2026-07-23 09:46:20 +0800

    Create Number229_Solution1.java

---
+++ src/day05/Number229_Solution1.java
@@ -0,0 +1,22 @@
+package day05;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Number229_Solution1 {
+    public List<Integer> majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int num : nums){
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        List<Integer> list = new ArrayList<>();
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+            if (entry.getValue() > nums.length / 3){
+                list.add(entry.getKey());
+            }
+        }
+        return list;
+    }
+}
