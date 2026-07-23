afdd4420442db37ef3719ce1cb26c8f8ac7b08d0
岁年 <beayoqutr@gmail.com>
2026-07-23 09:02:13 +0800

    Create Number169_Solution1.java

---
+++ src/day05/Number169_Solution1.java
@@ -0,0 +1,19 @@
+package day05;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Number169_Solution1 {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            if (entry.getValue() > nums.length / 2) {
+                return entry.getKey();
+            }
+        }
+        return -1;
+    }
+}
