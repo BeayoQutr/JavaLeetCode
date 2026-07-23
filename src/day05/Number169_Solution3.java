7eed3508b94ea56bdf03f26b1aa98866976df46e
岁年 <beayoqutr@gmail.com>
2026-07-23 09:31:16 +0800

    Create Number169_Solution3.java

---
+++ src/day05/Number169_Solution3.java
@@ -0,0 +1,17 @@
+package day05;
+
+public class Number169_Solution3 {
+    public int majorityElement(int[] nums) {
+        int count = 0;
+        int candidate = 0;
+
+        for (int num : nums) {
+            if (count == 0) {
+                candidate = num;
+            }
+            count += (num == candidate) ? 1 : -1;
+        }
+
+        return candidate;
+    }
+}
